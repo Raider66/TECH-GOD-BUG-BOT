d3b3b315ae5537724dc9f755184392b8e325089c
Raider66 <ceelgeeventures66@gmail.com>
2024-08-05 12:21:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '2347019733982'  //creator number
+global.ownername = 'Lord ダークⓘ' //owner name
+global.botname = 'ダーク' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
